422e4b1fad957a87920538446a0f9375609a8b46
James Petit <jp.james.petit@gmail.com>
2016-03-12 14:58:45 +0100

    Voila un script.

---
+++ TextBox.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class TextBox : MonoBehaviour 
+{
+	public Coroutine co;
+	public bool typing;
+	public float pause = 0.1f;
+	public AudioClip dialogue;
+	public AudioSource source;
+	public List<string> messages = new List<string>();
+	public int i = 0;
+	
+	void Update () 
+	{
+		if(Input.GetKeyDown(KeyCode.X))
+		{
+			StopCoroutine(co);
+			typing = false;
+			GetComponent<Text>().text = (messages[i]); 
+		}
+
+		if(Input.GetKeyDown(KeyCode.Z) && typing == false)
+		{
+			StopCoroutine(co);
+			GetComponent<Text>().text = ""; 
+			i++;
+			if(i >= messages.Count)
+			{
+				messages.Clear();
+				i = 0;
+				transform.parent.parent.gameObject.SetActive(false);
+				return;
+			}
+			co = StartCoroutine(TypeLetters(messages[i]));
+		}
+
+		if(Input.GetKeyDown(KeyCode.C))
+		{
+			StopCoroutine(co);
+			messages.Clear();
+			GetComponent<Text>().text = ""; 
+			i = 0;
+			transform.parent.parent.gameObject.SetActive(false);
+			return;
+		}
+	}
+
+	public IEnumerator TypeLetters (string currentMessage) 
+	{
+		typing = true;
+		foreach (char letter in currentMessage.ToCharArray()) 
+		{
+			GetComponent<Text>().text += letter; 
+			if(letter != " "[0] && letter != "."[0] && letter != "?"[0] && letter != "!"[0])
+			{
+				source.PlayOneShot(dialogue,1);
+			}
+			yield return new WaitForSeconds(pause);
+		}
+		typing = false;
+	}
+}
